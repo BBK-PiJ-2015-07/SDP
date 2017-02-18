69d2527a83cf196d46e82411eeffe5b36b446b6d
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-02-18 20:44:53 +0000

    week01 worksheet 2 - added method to find matching constructors based on user input

--- exercises/week01/worksheet02/src/ObjectFactory.java
+++ exercises/week01/worksheet02/src/ObjectFactory.java
@@ -1,8 +1,11 @@
 package worksheet02.src;
 
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+import scala.xml.Null;
+
+import java.lang.reflect.Constructor;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by Case on 18/02/2017.
@@ -11,7 +14,11 @@ public class ObjectFactory {
 
     public static void main(String[] args){
         String[] input = getInput();
+        List<Constructor> cs = getMatchingConstructors(input);
+        /*for(Constructor c : cs){
 
+            System.out.println("-- " + c);
+        }*/
     }
 
 
@@ -24,4 +31,32 @@ public class ObjectFactory {
         String[] strs = input.split("\\s*,\\s*");
         return  strs;
     }
+
+    /**
+     *  Returns a list of constructors of a class that match the parameter count.
+     * @param input The first element in the input String[] is the name of the class, the rest are arguments to the constructor.
+     */
+    private static List<Constructor> getMatchingConstructors(String[] input){
+
+        Class cls = null;
+        try {
+            cls = Class.forName(input[0]);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        List<Constructor> cs  = null;
+        try {
+             cs = Arrays.asList(cls.getConstructors());
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+        List<Constructor> cs2 = cs.stream()
+                .filter(c -> c.getParameterCount() == input.length - 1)
+                .collect(Collectors.toList());
+
+        return cs2;
+    }
+
 }
