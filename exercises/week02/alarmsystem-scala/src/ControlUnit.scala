a51d2b1be53ac24bf60ee57f84f19808a733831a
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-02-23 13:11:44 +0000

    week02 - lab - trying constructor dependency injection for ControlUnit

--- exercises/week02/alarmsystem-scala/src/ControlUnit.scala
+++ exercises/week02/alarmsystem-scala/src/ControlUnit.scala
@@ -1,10 +1,18 @@
 import scala.collection.mutable.ListBuffer
 
-class ControlUnit {
+class ControlUnit (sensors: ListBuffer[Sensor]){
+
+  private var sensors: ListBuffer[Sensor] = _
+
+  ControlUnit(sensors: ListBuffer[Sensor]){
+    this.sensors = sensors
+  }
+
   def pollSensors() {
-    val sensors = new ListBuffer[Sensor]()
+    /*val sensors = new ListBuffer[Sensor]()
     sensors += new FireSensor()
-    sensors += new SmokeSensor()
+    sensors += new SmokeSensor()*/
+
     for (sensor <- sensors) {
       if (sensor.isTriggered) {
         System.out.println("A " + sensor.getSensorType + " sensor was triggered at " + sensor.getLocation)
