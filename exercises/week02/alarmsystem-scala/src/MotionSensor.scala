fcd9a4fdf8cae418ea3b8185e79332036dba3830
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-02-23 13:37:52 +0000

    week02 - lab - first shot at MotionSensor class

--- exercises/week02/alarmsystem-scala/src/MotionSensor.scala
+++ exercises/week02/alarmsystem-scala/src/MotionSensor.scala
@@ -1,13 +1,26 @@
+import scala.util.Random
+
 /**
   * Created by Case on 23/02/2017.
   */
 class MotionSensor extends Sensor{
 
-  override def isTriggered: Boolean = ???
+  private var _location: String = _
+  private var _battery: Double = _
+  private val _sensorType: String = "Motion Sensor"
+  private val _isHazardSensor = false
 
-  override def getLocation: String = ???
+  def MotionSensor(location: String, battery: Double) ={
+    this._location = location
+    this._battery = battery
+  }
 
-  override def getSensorType: String = ???
+  //Say triggered 5% of the time
+  override def isTriggered: Boolean = Random.nextInt(100) < 5
 
-  override def isHazardSensor: Boolean = ???
+  override def getLocation: String = _location
+
+  override def getSensorType: String = _sensorType
+
+  override def isHazardSensor: Boolean = _isHazardSensor
 }
