29ccbd9972ae661979074df08e307bd5d86494f8
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-02-25 16:38:02 +0000

    IconstImpl done

--- src/main/scala/ByteCodeImpl/IconstImpl.scala
+++ src/main/scala/ByteCodeImpl/IconstImpl.scala
@@ -5,13 +5,14 @@ import vm.VirtualMachine
 /**
   * Created by davidasfaha on 25/02/2017.
   */
-class IconstImpl extends ByteCode{
+class IconstImpl(value: Int) extends ByteCode{
+
   /**
     * A unique byte value representing the bytecode. An implementation
     * will set this to the bytecode corresponding to the name of the
     * bytecode in [[ByteCodeValues]]
     */
-  override val code: Byte = bytecode.get(1)
+  override val code: Byte = bytecode.get("iconst").get
 
   /**
     * Returns a new [[VirtualMachine]] after executing this bytecode operation.
@@ -19,5 +20,7 @@ class IconstImpl extends ByteCode{
     * @param vm the initial virtual machine
     * @return a new virtual machine
     */
-  override def execute(vm: VirtualMachine): VirtualMachine = ???
+  override def execute(vm: VirtualMachine): VirtualMachine = vm.push(value)
+
+  }
 }
