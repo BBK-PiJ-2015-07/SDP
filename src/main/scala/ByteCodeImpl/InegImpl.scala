ca264d086c05406d269d5eb8e713eeec839f93bb
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-02-25 17:10:35 +0000

    done some ByteCode implementations

--- src/main/scala/ByteCodeImpl/InegImpl.scala
+++ src/main/scala/ByteCodeImpl/InegImpl.scala
@@ -1,18 +1,18 @@
 package ByteCodeImpl
 
-import bc.{ByteCode, ByteCodeValues}
+import bc.ByteCode
 import vm.VirtualMachine
-/**
-  * Created by davidasfaha on 25/02/2017.
-  */
-class IconstImpl(value: Int) extends ByteCode{
 
+/**
+  * Created by Case on 25/02/2017.
+  */
+class InegImpl extends ByteCode {
   /**
     * A unique byte value representing the bytecode. An implementation
     * will set this to the bytecode corresponding to the name of the
-    * bytecode in [[ByteCodeValues]]
+    * bytecode in [[bc.ByteCodeValues]]
     */
-  override val code: Byte = bytecode.get("iconst").get
+  override val code: Byte = bytecode.get("ineg").get
 
   /**
     * Returns a new [[VirtualMachine]] after executing this bytecode operation.
@@ -20,7 +20,8 @@ class IconstImpl(value: Int) extends ByteCode{
     * @param vm the initial virtual machine
     * @return a new virtual machine
     */
-  override def execute(vm: VirtualMachine): VirtualMachine = vm.push(value)
-
-
+  override def execute(vm: VirtualMachine): VirtualMachine = {
+    var (x: Int, vm1: VirtualMachine) = vm.pop()
+    vm.push(-x)
+  }
 }
