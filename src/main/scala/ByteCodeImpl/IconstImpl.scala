6ad66c04406e7cc5cc08bd4743fee57f3e414d69
David Asfaha <davidasfaha@gmail.com>
2017-02-25 16:23:03 +0000

    Started first bytecode

---
+++ src/main/scala/ByteCodeImpl/IconstImpl.scala
@@ -0,0 +1,23 @@
+package ByteCodeImpl
+
+import bc.{ByteCode, ByteCodeValues}
+import vm.VirtualMachine
+/**
+  * Created by davidasfaha on 25/02/2017.
+  */
+class IconstImpl extends ByteCode{
+  /**
+    * A unique byte value representing the bytecode. An implementation
+    * will set this to the bytecode corresponding to the name of the
+    * bytecode in [[ByteCodeValues]]
+    */
+  override val code: Byte = bytecode.get(1)
+
+  /**
+    * Returns a new [[VirtualMachine]] after executing this bytecode operation.
+    *
+    * @param vm the initial virtual machine
+    * @return a new virtual machine
+    */
+  override def execute(vm: VirtualMachine): VirtualMachine = ???
+}
