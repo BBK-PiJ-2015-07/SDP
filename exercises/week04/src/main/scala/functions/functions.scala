84b142ea0e281b4cef0976c7e4fcbc17681dd629
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-03-02 18:56:51 +0000

    Added foldLeft implementations to Funcs

--- exercises/week04/src/main/scala/functions/functions.scala
+++ exercises/week04/src/main/scala/functions/functions.scala
@@ -92,31 +92,45 @@ object Funcs {
     * the sublists into one long list. For example, flatten(List(List(1,2,3),
     * List(4,5,6))) produces List(1,2,3,4,5,6).
     */
-  def sum(ls: List[Double]): Double = ls match {
+  /*def sum(ls: List[Double]): Double = ls match {
     case Nil => 0
     case hd :: tl => hd + sum(tl)
-  }
+  }*/
 
-  def product(ls: List[Double]): Double = ls match {
+ def sum(ls: List[Double]): Double = foldLeft(ls, 0.0)((x,y) => x + y)
+
+  /*def product(ls: List[Double]): Double = ls match {
     case Nil => 1
     case hd :: tl => hd * product(tl)
-  }
+  }*/
 
-  def length[A](ls: List[A]): Int = ls match {
+  def product(ls: List[Double]): Double = foldLeft(ls, 1.0)((x,y) => x*y)
+
+
+  /*def length[A](ls: List[A]): Int = ls match {
     case Nil => 0
     case hd :: tl => 1 + length(tl)
-  }
+  }*/
 
-  def reverse[A](ls: List[A]): List[A] = ls match {
+  def length[A](ls: List[A]): Int = foldLeft(ls, 0)((sum,_) => sum + 1)
+
+  /*def reverse[A](ls: List[A]): List[A] = ls match {
     case Nil => Nil
     case hd :: tl => reverse(tl) ::: List(hd)
-  }
+  }*/
 
-  def flatten[A](ls: List[A]): List[A] = ls match {
+  def reverse[A](ls: List[A]): List[A] = foldLeft(ls, List[A]()) ((x, y) => y :: x )
+
+
+
+  /*def flatten[A](ls: List[A]): List[A] = ls match {
     case Nil => Nil
     case (head: List[A]) :: tail => flatten(head) ::: flatten(tail)
     case head :: tail => head :: flatten(tail)
-  }
+  }*/
+
+  def flatten[A](ls: List[List[A]]): List[A] = foldLeft(ls, List[A]()) ((x, y) => x++y)
+
 
   // MAP AND FILTER
 
