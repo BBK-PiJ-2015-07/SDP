358693fcd900db7e2e86ff9557fe8cf92ea36473
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-04-12 16:30:40 +0100

    Week 5 - reimplemented Counter as a case class with default value 0

--- exercises/week05/src/main/scala/scala-exercises/counter/Counter.scala
+++ exercises/week05/src/main/scala/scala-exercises/counter/Counter.scala
@@ -3,10 +3,13 @@ package counter
 /**
   * Created by apinter on 30/03/2017.
   */
-class Counter(ct: Option[Int]) {
-  var count = ct getOrElse 1
+case class Counter(ct: Option[Int]) {
+  val count = ct getOrElse 0
 
-  def inc() = { new Counter(Option(count + 1)) }
+  /*def inc() = { new Counter(Option(count + 1)) }
   def dec() = { new Counter(Option(count - 1)) }
+*/
 
+  def inc() = { this.copy(Option(count + 1)) }
+  def dec() = { this.copy(Option(count - 1)) }
 }
