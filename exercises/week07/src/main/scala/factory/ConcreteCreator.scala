858b2f8de6dc52ce78e247bc89a34d0714b1a5ba
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-04-17 19:33:04 +0100

    Week07 - Added Factory classes: Product, ConcreteProduct, Creator, ConcreteCreator

---
+++ exercises/week07/src/main/scala/factory/ConcreteCreator.scala
@@ -0,0 +1,8 @@
+package factory
+
+/**
+  * Created by Case on 17/04/2017.
+  */
+class ConcreteCreator {
+
+}
---
+++ exercises/week07/src/main/scala/factory/Product.scala
@@ -0,0 +1,9 @@
+package factory
+
+/**
+  * Created by Case on 17/04/2017.
+  */
+trait Product {
+  def getProductName: String
+  def getProductCode: Int
+}
