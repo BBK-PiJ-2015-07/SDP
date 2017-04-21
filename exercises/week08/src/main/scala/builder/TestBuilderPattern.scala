5be71a8520c3c667d186e4564b91c9366b886f01
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-04-21 17:44:49 +0100

    Week 8 - Builder - SedanCarBuilder only builds one instance

--- exercises/week08/src/main/scala/builder/TestBuilderPattern.scala
+++ exercises/week08/src/main/scala/builder/TestBuilderPattern.scala
@@ -5,13 +5,25 @@ object TestBuilderPattern {
     var carBuilder: CarBuilder = SedanCarBuilder
     var director: CarDirector = CarDirector(carBuilder)
 
-    director.build
-    println(carBuilder.getCar)
 
-    carBuilder = SportsCarBuilder
+    director.build
+    //println(carBuilder.getCar)
+
+    val car1: Car = carBuilder.getCar
+
+    director.build
+
+    val car2: Car = carBuilder.getCar
+
+
+    println("car1 hash: " + car1.hashCode())
+    println("car2 hash: " + car2.hashCode())
+
+
+    /*carBuilder = SportsCarBuilder
     director = CarDirector(carBuilder)
 
     director.build
-    println(carBuilder.getCar)
+    println(carBuilder.getCar)*/
   }
 }
