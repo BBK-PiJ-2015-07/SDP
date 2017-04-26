902b2160436cf871d4162fcad914a681f0d12405
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-04-26 23:38:01 +0100

    Week10 - chain done.

--- exercises/week10/src/main/scala/chain/ExcelFileHandler.scala
+++ exercises/week10/src/main/scala/chain/ExcelFileHandler.scala
@@ -1,9 +1,21 @@
 package chain
 
 case class ExcelFileHandler(s: String) extends Handler {
-  override def setHandler(handler: Handler): Unit = ???
 
-  override def process(file: File): Unit = ???
+  var handler: Option[Handler] = None
+  val handlerStr = " Handler"
+  val handlerStrLength = handlerStr.length()
+  val Str = getHandlerName.dropRight(handlerStrLength).toLowerCase()
 
-  override def getHandlerName(): String = ???
+  override def setHandler(handler: Handler): Unit = this.handler = Some(handler)
+
+  override def process(file: File): Unit = file.fileType match {
+    case Str => println("Process and saving " + Str + " file... by " + getHandlerName())
+    case _ => handler match {
+      case Some(handler) => println(getHandlerName() + " forwards the request to " + handler.getHandlerName()); handler.process(file)
+      case None => println("File not supported")
+    }
+  }
+
+  override def getHandlerName(): String = this.s
 }
--- exercises/week10/src/main/scala/chain/VideoFileHandler.scala
+++ exercises/week10/src/main/scala/chain/VideoFileHandler.scala
@@ -1,9 +1,29 @@
 package chain
 
+/**
+  * File handler
+  * @param s The name of the Handler. Format should be filetype + " Handler"
+  */
 case class VideoFileHandler(s: String) extends Handler {
-  override def setHandler(handler: Handler): Unit = ???
 
-  override def process(file: File): Unit = ???
+  var handler: Option[Handler] = None
 
-  override def getHandlerName(): String = ???
+  /*
+  //The following fields along with the class naming convention described in the javadoc could be used to create a general Handler implementation.
+  //The first pattern match in the process() function would match against Str
+  val handlerStr = " Handler"
+  val handlerStrLength = handlerStr.length()
+  val Str = getHandlerName.dropRight(handlerStrLength).toLowerCase()*/
+
+  override def setHandler(handler: Handler): Unit = this.handler = Some(handler)
+
+  override def process(file: File): Unit = file.fileType match {
+    case "video" => println("Process and saving video file... by " + getHandlerName())
+    case _ => handler match {
+      case Some(handler) => println(getHandlerName() + " forwards the request to " + handler.getHandlerName()); handler.process(file)
+      case None => println("File not supported")
+    }
+  }
+
+  override def getHandlerName(): String = this.s
 }
