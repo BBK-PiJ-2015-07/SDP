a2606c5edd6a55a85ee90b997a9d22bb24d2bb25
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-04-27 15:37:03 +0100

    Week10 - prototype pattern. done toString override for User class

--- exercises/week10/src/main/scala/prototype/AccessControlProvider.scala
+++ exercises/week10/src/main/scala/prototype/AccessControlProvider.scala
@@ -2,18 +2,18 @@ package prototype
 
 object AccessControlProvider {
 
-  private var map = ???
+  private var map = new Map()
 
   println("Fetching data from external resources and creating access control objects...")
 
   // UNCOMMENT
-  //map.put("USER", new AccessControl("USER", "DO_WORK"))
+  map.put("USER", new AccessControl("USER", "DO_WORK"))
 
-  //map.put("ADMIN", new AccessControl("ADMIN", "ADD/REMOVE USERS"))
+  map.put("ADMIN", new AccessControl("ADMIN", "ADD/REMOVE USERS"))
 
-  //map.put("MANAGER", new AccessControl("MANAGER", "GENERATE/READ REPORTS"))
+  map.put("MANAGER", new AccessControl("MANAGER", "GENERATE/READ REPORTS"))
 
-  //map.put("VP", new AccessControl("VP", "MODIFY REPORTS"))
+  map.put("VP", new AccessControl("VP", "MODIFY REPORTS"))
 
   def getAccessControlObject(controlLevel: String): AccessControl = ???
 
