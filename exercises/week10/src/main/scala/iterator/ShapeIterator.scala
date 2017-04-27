cf046b2c42a856e7cfe28fcaf1fab4ac9f6ba77c
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-04-27 18:01:42 +0100

    Week10 - iterator pattern. Iterator[Shape] to implement

--- exercises/week10/src/main/scala/iterator/ShapeIterator.scala
+++ exercises/week10/src/main/scala/iterator/ShapeIterator.scala
@@ -1,12 +1,15 @@
 package iterator
 
-import java.util.Iterator
+//import java.util.Iterator
 
 class ShapeIterator(private var shapes: Array[Shape]) extends Iterator[Shape] {
 
-  override def hasNext(): Boolean = ???
+  override def hasNext: Boolean = Iterator(shapes) hasNext
+    /*val it = Iterator(shapes)
+    it.hasNext
+  }*/
 
-  override def next(): Shape = ???
+  override def next: Shape = Iterator(shapes) next
 
-  override def remove(): Unit = ???
+  override def remove: Unit = Iterator(shapes)remove()
 }
