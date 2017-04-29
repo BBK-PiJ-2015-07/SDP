f2ebbaf2df0d60b8c3a81a569ee0f04276f74700
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-04-29 11:12:27 +0100

    Week11 - memento pattern done

--- exercises/week11/src/main/scala/memento/Originator.scala
+++ exercises/week11/src/main/scala/memento/Originator.scala
@@ -10,15 +10,28 @@ case class Originator(
 
   createSavepoint("INITIAL")
 
-  def createSavepoint(savepointName: String): Unit = ???
+  def createSavepoint(savepointName: String): Unit = {
+    println("Saving state..." + savepointName)
+    careTaker.saveMemento(Memento(x,y), savepointName)
+    lastUndoSavepoint = savepointName
+  }
 
-  def undo(): Unit = ???
+  def undo(): Unit = undo(lastUndoSavepoint)
 
-  def undo(savepointName: String): Unit = ???
+  def undo(savepointName: String): Unit = {
+    println("Undo at..." + savepointName)
+    val memento = careTaker.getMemento(savepointName)
+    x = memento.x
+    y = memento.y
+    setOriginatorState(savepointName)
+  }
 
-  def undoAll(): Unit = ???
+  def undoAll(): Unit = {
+    undo("INITIAL")
+    careTaker.clearSavepoints()
+  }
 
-  private def setOriginatorState(savepointName: String): Unit = ???
+  private def setOriginatorState(savepointName: String): Unit = lastUndoSavepoint = savepointName
 
   override def toString(): String = "X: " + x + ", Y: " + y
 }
